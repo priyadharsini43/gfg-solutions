1cfa5c3ba3c55fd1e8cbb6a557a55eddc212afed
Priyadharsini P <priyadharsini162@gmail.com>
2026-03-07 21:44:42 +0530

    Create subarraySum.java

---
+++ subarraySum.java
@@ -0,0 +1,21 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int start = 0;
+        int sum = 0;
+        for(int end = 0; end < arr.length; end++){
+            sum += arr[end];
+            while(sum > target && start <= end){
+                sum -= arr[start];
+                start++;
+            }   
+            if(sum == target){
+                result.add(start + 1); 
+                result.add(end + 1);   
+                return result;
+            }
+        }
+        result.add(-1);
+        return result;
+    }
+}
