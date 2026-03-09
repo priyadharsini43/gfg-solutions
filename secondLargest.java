1fecf4d37e182e61ed2cfccad991534e0b546275
Priyadharsini P <priyadharsini162@gmail.com>
2026-03-09 19:26:51 +0530

    Update secondLargest.java

--- secondLargest.java
+++ secondLargest.java
@@ -1,21 +1,16 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
-        
         int largest = -1;
         int secondLargest = -1;
-        
-        for(int i = 0; i < arr.length; i++){
-            
+        for(int i = 0; i < arr.length; i++){     
             if(arr[i] > largest){
                 secondLargest = largest;
                 largest = arr[i];
             }
-            
             else if(arr[i] > secondLargest && arr[i] != largest){
                 secondLargest = arr[i];
             }
-        }
-        
+        }   
         return secondLargest;
     }
 }
